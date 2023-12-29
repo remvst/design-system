76758858f323a8c76122ca308659015b56f956be
remvst <remvst@gmail.com>
2023-12-28 23:56:56 -0500

    More cleanup

--- demo/.storybook/preview.tsx
+++ demo/.storybook/preview.tsx
@@ -2,15 +2,6 @@ import { Preview } from '@storybook/react';
 import React from 'react';
 
 const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
   decorators: [
     (Story) => (
         <div style={{height: '100%'}}>
