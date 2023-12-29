5eef859140ba183fbd4f42eada80faf78e099c03
remvst <remvst@gmail.com>
2023-12-29 01:06:29 -0500

    Early typography

---
+++ demo/src/stories/Label.stories.tsx
@@ -0,0 +1,26 @@
+import { SmallLabel, LargeLabel, TinyLabel } from "@remvst/design-system";
+import React from "react";
+import { StoryObj } from "@storybook/react";
+
+export default {
+  title: "Utils/Label",
+};
+
+interface Args {
+  label: string;
+}
+
+export const Tiny: StoryObj<Args> = {
+  args: { label: 'Tiny label' },
+  render: (args) => <TinyLabel>{args.label}</TinyLabel>,
+};
+
+export const Small: StoryObj<Args> = {
+  args: { label: 'Small label' },
+  render: (args) => <SmallLabel>{args.label}</SmallLabel>,
+};
+
+export const Large: StoryObj<Args> = {
+  args: { label: 'Large label' },
+  render: (args) => <LargeLabel>{args.label}</LargeLabel>,
+};
